a122035e1b013ac0f1dd72e0bad2c8b6e46f0835
nicoalegria11 <nfalegria@uc.cl>
2021-05-24 18:20:12 -0400

    consulta2

--- Sites/index.php
+++ Sites/index.php
@@ -7,8 +7,22 @@
   <br>
 
   <h3 align="center"> Consulta1 </h3>
+  <p style="text-align:center;">Se va a mostrar el nombre de todas las tiendas, junto con los nombres de las comunas a cuales realizan despachos.</p>
 
   <form align="center" action="consultas/consulta1.php" method="post">
+    <br/><br/>
+    <input type="submit" value="Buscar">
+  </form>
+
+  <br>
+  <br>
+  <br>
+
+  <h3 align="center"> Consulta 2 </h3>
+  <p style="text-align:center;"> Ingrese una comuna. Se van a mostrar todos los jefes de tiendas ubicadas en dicha comuna.</p>
+  <form align="center" action="consultas/consulta2.php" method="post">
+    Nombre_Comuna:
+    <input type="text" name="comuna_elegida">
     <br/><br/>
     <input type="submit" value="Buscar">
   </form>
