a54d719790f3b77a0d40f80cbe84eda84ca8c0c0
nicoalegria11 <nfalegria@uc.cl>
2021-05-24 19:29:18 -0400

    consulta3j

--- Sites/consultas/consulta3.php
+++ Sites/consultas/consulta3.php
@@ -8,11 +8,11 @@
   $var = $_POST["tipo"];
 
     if ($var == "no comestible") {
-    $query = "SELECT DISTINCT tiendas.tid, tiendas.tnombre FROM tiendas, tienda_vende, productos WHERE tienda_vende.tid = tiendas.tid AND tienda_vende.pid = productos.pid AND productos.ptipo LIKE '%$var%';";
+    $query = "SELECT DISTINCT tiendas.tid, tiendas.tnombre FROM tiendas, tienda_vende, productos WHERE tienda_vende.tid = tiendas.tid AND tienda_vende.pid = productos.pid AND productos.ptipo = $var ORDER BY tiendas.tid;";
     } elseif ($var == "comestible") {
-    $query = "SELECT DISTINCT tiendas.tid, tiendas.tnombre FROM tiendas, tienda_vende, productos WHERE tienda_vende.tid = tiendas.tid AND tienda_vende.pid = productos.pid AND productos.ptipo LIKE '%$var%';";
+    $query = "SELECT DISTINCT tiendas.tid, tiendas.tnombre FROM tiendas, tienda_vende, productos WHERE tienda_vende.tid = tiendas.tid AND tienda_vende.pid = productos.pid AND productos.ptipo = $var ORDER BY tiendas.tid;";
     } else {
-    $query = "SELECT DISTINCT tiendas.tid, tiendas.tnombre FROM tiendas, comestibles, tienda_vende WHERE tienda_vende.tid = tiendas.tid AND tienda_vende.pid = comestibles.pid AND comestibles.categoria LIKE '%$var%';";
+    $query = "SELECT DISTINCT tiendas.tid, tiendas.tnombre FROM tiendas, comestibles, tienda_vende WHERE tienda_vende.tid = tiendas.tid AND tienda_vende.pid = comestibles.pid AND comestibles.categoria = $var ORDER BY tiendas.tid;";
     }
 
   $result = $db -> prepare($query);
