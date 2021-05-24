b11b25fe123561a4a83427281310c09ad69d616a
nicoalegria11 <nfalegria@uc.cl>
2021-05-24 17:51:09 -0400

    weaindex2

--- Sites/index.php
+++ Sites/index.php
@@ -1,1 +1,7 @@
 <?php include('templates/header.html');   ?>
+
+<body>
+  <h1 align="center"> ENTREGA 2 </h1>
+  <p style="text-align:center;">Aquí podrás encontrar información de nuestra base de datos.</p>
+
+  <br>
