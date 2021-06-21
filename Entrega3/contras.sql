53deaa5104ea2b6cff0e4a1b6ee5bb9ec91fd38e
nicoalegria11 <nfalegria@uc.cl>
2021-06-21 13:02:40 -0400

    aa

--- Entrega3/contras.sql
+++ Entrega3/contras.sql
@@ -7,19 +7,12 @@ contras (uid int, unombre varchar(100), rut varchar(100), edad int, sexo varchar
 -- declaramos lo que retorna, en este caso un booleano
 RETURNS VOID AS $$
 
-DECLARE
-idmax int;
-
 -- definimos nuestra función
 BEGIN
-    SELECT INTO idmax
-    MAX(uid)
-    FROM usuarios;
-
     -- verificar si existe la columna contrasena, si no existe la agregamos y seteamos la contraseña aleatoriamente
     IF 'contrasena' NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='usuarios') THEN
         ALTER TABLE usuarios ADD contrasena varchar(100);
-        UPDATE usuarios SET contrasena = (SELECT substring(usuarios.rut, 0, 4) FROM usuarios WHERE usuarios.uid = idmax);
+        UPDATE usuarios SET contrasena = (SELECT substring(usuarios.rut, 0, 4) FROM usuarios LIMIT 1);
     END IF;
 
 
