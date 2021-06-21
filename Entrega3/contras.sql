6306e4919a389216b6ff9b784270c22bd423a05d
nicoalegria11 <nfalegria@uc.cl>
2021-06-21 16:07:04 -0400

    aa

--- Entrega3/contras.sql
+++ Entrega3/contras.sql
@@ -9,8 +9,8 @@ RETURNS VOID AS $$
 
 -- definimos nuestra función
 BEGIN
-    -- verificar si existe la columna contrasena, si no existe la agregamos y seteamos la contraseña aleatoriamente
-    UPDATE usuarios SET contrasena = contra WHERE usuarios.uid = uid AND usuarios.rut = rut;
+    -- seteamos la contraseña hecha aleatoriamente
+    UPDATE usuarios SET contraseña = contra WHERE usuarios.uid = uid AND usuarios.rut = rut;
 
 
 -- finalizamos la definición de la función y declaramos el lenguaje
