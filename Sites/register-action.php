c3cc5a954dec11a0d6e2aeb23be26b2aee6fe4f5
CRISTOBAL <cafernandez8@uc.cl>
2021-06-21 17:45:40 -0400

    fix para registrarse

--- Sites/register-action.php
+++ Sites/register-action.php
@@ -9,7 +9,10 @@ $edad = $_POST['edad'];
 $sexo = $_POST['sex'];
 $password = $_POST['password'];
 
-$register = $db->query("INSERT INTO usuarios (unombre, rut, edad, sexo, contrasena) VALUES ('$username', '$rut', '$edad', '$sexo', '". md5($password)."')");
+$result = $db -> query("SELECT uid FROM usuarios ORDER BY uid DESC Limit 1");
+$ultimo_id = $result -> fetchAll(); 
+
+$register = $db->query("INSERT INTO usuarios (uid, unombre, rut, edad, sexo, contrasena) VALUES ('$ultimo_id[0][0] + 1' ,'$username', '$rut', '$edad', '$sexo', '". md5($password)."')");
 if ($register) {
 header("Location: registration.php?register_action=success");
 }
