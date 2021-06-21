93bc0dd916c479dc27a63d391c6a1192238c9fd8
nicoalegria11 <nfalegria@uc.cl>
2021-06-20 20:21:16 -0400

    aa

--- Entrega3/contras.sql
+++ Entrega3/contras.sql
@@ -20,7 +20,7 @@ BEGIN
     -- si el uid en el argumento no está en la tabla, agregamos el usuario
     -- notar que ahora debemos agregar el dato de la columna generation en el values a insertar
     IF uid NOT IN (SELECT uid from usuarios) THEN
-        INSERT INTO usuarios values(uid, unombre, rut, edad, sexo, did, 100);
+        INSERT INTO usuarios values(uid, unombre, rut, edad, sexo, 100);
 
         -- retornamos true si se agregó el valor
         RETURN TRUE;
--- Sites/index.php
+++ Sites/index.php
@@ -1,8 +1,8 @@
 <?php include('templates/header.html');   ?>
 
 <body>
-<h3>Ver base de datos 1</h3>
-    <form  action='./consultas/procedimiento_contrasena.php' method='GET'>
+<h3>Ver procedimiento 1</h3>
+    <form  action='./consultas/procedimiento1.php' method='GET'>
         <input class='btn' type='submit' value='Consultar'>
     </form>
 </body>
