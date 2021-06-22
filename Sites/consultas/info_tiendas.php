6c2457c71a7b1211cc9fd60b321f7abec0e9dae9
nicoalegria11 <nfalegria@uc.cl>
2021-06-22 18:50:23 -0400

    aa

--- Sites/consultas/info_tiendas.php
+++ Sites/consultas/info_tiendas.php
@@ -1,4 +1,13 @@
-<?php session_start();
-include('templates/header.html');
-require("config/conexion.php");
+<?php
+
+    // Nos conectamos a las bdds
+    require("../config/conexion.php");
+    include('../templates/header.html');
+?>
+
+<?php
+$tienda = $_POST['Tienda'];
+$lista = explode(":", $tienda);
+echo $lista[0];
+echo $lista[1];
 ?>
