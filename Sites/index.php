e7516f8d3cc891d6fb4bfb1ffa7d9b17c207480a
nicoalegria11 <nfalegria@uc.cl>
2021-06-22 16:43:46 -0400

    cambioindex

--- Sites/index.php
+++ Sites/index.php
@@ -29,16 +29,6 @@ require("config/conexion.php");
   ?>
 
   <br>
-<h3>Ver procedimiento 1</h3>
-    <form  action='./consultas/procedimiento1.php' method='GET'>
-        <input class='btn' type='submit' value='Consultar'>
-    </form>
-
-<h3>Ver procedimiento 2</h3>
-    <form  action='./consultas/procedimiento2.php' method='GET'>
-        <input class='btn' type='submit' value='Consultar'>
-    </form>
-
 <br>
 <br>
 <br>
