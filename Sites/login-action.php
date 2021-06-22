ab558ed08593c6783c384592d9070d8bcde11102
CRISTOBAL <cafernandez8@uc.cl>
2021-06-21 22:07:31 -0400

    cambio en el login

--- Sites/login-action.php
+++ Sites/login-action.php
@@ -7,12 +7,12 @@ $rut = $_POST['rut'];
 $password = $_POST['password'];
 $query = "SELECT * FROM usuarios WHERE rut = '$rut' AND password = '".md5($password)."'";
 $result = $db -> prepare($query);
-$result -> execute()
-$inf_usuario = $result -> fetchAll()
+$result -> execute();
+$inf_usuario = $result -> fetchAll();
 if ($inf_usuario->num_rows <= 1) {
 $_SESSION_rut = $rut;
-$_SESSION_uid = $inf_usuario[0][1]
-$_SESSION_nombre = $inf_usuario[0][1]
+$_SESSION_uid = $inf_usuario[0][1];
+$_SESSION_nombre = $inf_usuario[0][1];
 
 header("Location: index.php");
 }
