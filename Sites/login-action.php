f1bf258bcd6f60098a9ababac06ef1bb92eb9a28
CRISTOBAL <cafernandez8@uc.cl>
2021-06-22 17:15:02 -0400

    fix del login-action

--- Sites/login-action.php
+++ Sites/login-action.php
@@ -6,7 +6,7 @@ if (isset($_POST['rut'])) {
 $rut = $_POST['rut'];
 $password = $_POST['password'];
 $query = "SELECT * FROM usuarios WHERE rut = '$rut' AND password = '".md5($password)."'";
-$result = $db1 -> prepare($query);
+$result = $db -> prepare($query);
 $result -> execute();
 $usuario = $result -> fetchAll();
 if ($password = $usuario[0][5]) {
