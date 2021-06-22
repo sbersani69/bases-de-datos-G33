049f762e41c1bdab82c8691eefb590f8146cf5e4
CRISTOBAL <cafernandez8@uc.cl>
2021-06-22 17:05:53 -0400

    fix del login-action

--- Sites/login-action.php
+++ Sites/login-action.php
@@ -6,7 +6,7 @@ if (isset($_POST['rut'])) {
 $rut = $_POST['rut'];
 $password = $_POST['password'];
 $login = $db->query("SELECT * FROM usuarios WHERE rut = '$rut' AND password = '$password'");
-if ($login->num_rows <= 1) {
+if ($login->num_rows = 1) {
 $_SESSION['rut'] = $rut;
 header("Location: index.php");
 }
