194f43479e153a032d0ba77cb11c09f3e461fb2f
CRISTOBAL <cafernandez8@uc.cl>
2021-06-22 22:29:05 -0400

    posible cambio de contraseña

--- Sites/cambiar_contraseña.php
+++ Sites/cambiar_contraseña.php
@@ -11,7 +11,7 @@ $old_pass = $_POST['old_pass'];
 $new_pass = $_POST['new_pass'];
 $current_pass = $row[0][0];
 
-if ($old_pass = $current_pass){
+if ($old_pass = $current_pass): {
   $query2 = "UPDATE usuarios SET contraseña = '$new_pass' WHERE rut = $id;";
   $change = $db -> prepare($query2);
   $change -> execute();
