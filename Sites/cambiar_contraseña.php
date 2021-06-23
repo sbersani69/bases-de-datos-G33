17037212e6189aff10b0e701c517af0d1c2f2036
CRISTOBAL <cafernandez8@uc.cl>
2021-06-22 22:05:54 -0400

    post cambiar contraseña

--- Sites/cambiar_contraseña.php
+++ Sites/cambiar_contraseña.php
@@ -2,10 +2,23 @@
   session_start();
   require("config/conexion.php");
 $id=$_SESSION['rut'];
+$query= "SELECT contraseña FROM usuarios where rut='$id'";
+$result = $db -> prepare($query);
+$result -> execute();
+$row= $result -> fetchAll();
 
 $old_pass = $_POST['old_pass'];
 $new_pass = $_POST['new_pass'];
 
-echo $old_pass;
-echo $new_pass;
+if ($old_pass = $row[0][0]){
+  $query2 = "UPDATE usuarios SET contraseña = '$new_pass' WHERE usuarios.uid = $id;";
+  $change = $db -> prepare($query2);
+  $change -> execute();
+  echo 'Contraseña cambiada exitosamente.';
+?>
+<form method="post" action="index.php?">
+  <input type="submit" value="Volver" />
+</form>
+<?php
+}
 ?>
