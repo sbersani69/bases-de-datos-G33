719b8fb5619a39365062dedca51cc362335d7f98
CRISTOBAL <cafernandez8@uc.cl>
2021-06-22 21:08:57 -0400

    perfil muestra direccion

--- Sites/profile.php
+++ Sites/profile.php
@@ -6,6 +6,13 @@ $query= "SELECT * FROM usuarios where rut='$id'";
 $result = $db -> prepare($query);
 $result -> execute();
 $row= $result -> fetchAll();
+$uid = $row[0][0];
+
+$query2 = "SELECT direccion FROM direcciones, direcciones_asociadas WHERE direcciones.did = direcciones_asociadas.did AND direcciones_asociadas.uid = '$uid';";
+$result2 = $db -> prepare($query);
+$result2 -> execute();
+$row2 = $result2 -> fetchAll();
+
   ?>
   <h1>Perfil del Usuario</h1>
 
@@ -13,45 +20,5 @@ $row= $result -> fetchAll();
   <label>RUT: <?php echo $row[0][2]; ?></label> <br>
   <label>Sexo: <?php echo $row[0][3]; ?></label> <br>
   <label>Edad: <?php echo $row[0][4]; ?></label> <br>
+  <label>Dirección: <?php echo $row2[0][0]; ?></label> <br>
 
-<div class="profile-input-field">
-        <h3>Actualizar Información</h3>
-        <form method="post" action="#" >
-          <div class="form-group">
-            <label>Nombre</label>
-            <input type="text" class="form-control" name="name" style="width:20em;" placeholder="Enter your Fullname" value="<?php echo $row['unombre']; ?>" required />
-          </div>
-          <div class="form-group">
-            <label>Sexo</label>
-            <input type="text" class="form-control" name="sex" style="width:20em;" placeholder="Enter your Gender" required value="<?php echo $row['sexo']; ?>" />
-          </div>
-          <div class="form-group">
-            <label>Edad</label>
-            <input type="number" class="form-control" name="age" style="width:20em;" placeholder="Enter your Age" value="<?php echo $row['edad']; ?>">
-          </div>
-          <div class="form-group">
-            <input type="submit" name="submit" class="btn btn-primary" style="width:20em; margin:0;"><br><br>
-            <center>
-             <a href="logout.php">Log out</a> <br>
-             <a href="index.php">Volver</a>
-           </center>
-          </div>
-        </form>
-      </div>
-      </html>
-      <?php
-      if(isset($_POST['submit'])){
-        $nombre = $_POST['name'];
-        $gender = $_POST['sex'];
-        $age = $_POST['age'];
-        $query = "UPDATE usuarios SET unombre = '$nombre', sexo = '$gender', age = $age WHERE rut = '$id';";
-        $result = $db -> prepare($query);
-        $result -> execute();
-                    ?>
-                     <script type="text/javascript">
-            alert("Update Successfull.");
-            window.location = "index.php";
-        </script>
-        <?php
-             }              
-?>
