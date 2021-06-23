fc8eb93b39a08d58dc7c5757c616650058679ab5
CRISTOBAL <cafernandez8@uc.cl>
2021-06-22 21:47:30 -0400

    post cambiar contraseña

--- Sites/cambiar_contraseña.php
+++ Sites/cambiar_contraseña.php
@@ -3,8 +3,8 @@
   require("config/conexion.php");
 $id=$_SESSION['rut'];
 
-$old_pass = $_POST['old_pass']
-$new_pass = $_POST['new_pass']
+$old_pass = $_POST['old_pass'];
+$new_pass = $_POST['new_pass'];
 
 echo $old_pass;
 echo $new_pass;
