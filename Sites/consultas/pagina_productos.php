9ee72bad96922858c2df72d692371506edf23def
CRISTOBAL <cafernandez8@uc.cl>
2021-07-04 16:30:33 -0400

    pag productos

--- Sites/consultas/pagina_productos.php
+++ Sites/consultas/pagina_productos.php
@@ -8,7 +8,16 @@
 <?php
 $producto = $_POST['Producto'];
 $lista = explode(":", $producto);
+
+$query = "SELECT * FROM productos WHERE pid = '$lista[0]';";
+$result = $db -> prepare($query);
+$result -> execute();
+$row = $result -> fetchAll();
 ?>
 <h3 align="center"> Producto: <?php echo $lista[1]; ?> con id: <?php echo $lista[0];?> </h3>
+
+<label>Precio: <?php echo $row[0][3]; ?></label> <br>
+<label>Descripción: <?php echo $row[0][4]; ?></label> <br>
+<label>Tipo: <?php echo $row2[0][0]; ?></label> <br>
 
 <?php include('../templates/footer.html'); ?>
